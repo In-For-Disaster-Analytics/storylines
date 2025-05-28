986c7df9004d5bba1dc566b4d580cb110276d68c
Maximiliano Osorio <maxiosorio@gmail.com>
2025-05-28 11:25:03 -0400

    enable expandable_segments:True
    ```
    torch.OutOfMemoryError: CUDA out of memory. Tried to allocate 90.00 MiB. GPU 0 has a total capacity of 39.39 GiB of which 63.94 MiB is free. Including non-PyTorch memory, this process has 39.32 GiB memory in use. Of the allocated memory 36.42 GiB is allocated by PyTorch, and 2.40 GiB is reserved by PyTorch but unallocated. If reserved but unallocated memory is large try setting PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True to avoid fragmentation.  See documentation for Memory Management  (https://pytorch.org/docs/stable/notes/cuda.html#environment-variables)
    ```

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -xe
 
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 cd ${_tapisExecSystemInputDir}
 insanely-fast-whisper --file-name ${_tapisExecSystemInputDir}/audio.mp3
