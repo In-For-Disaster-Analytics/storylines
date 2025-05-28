ff0ce7df38fa01f38b9e632b8382a5a5f363df48
Maximiliano Osorio <maxiosorio@gmail.com>
2025-05-28 11:53:22 -0400

    Revert "enable expandable_segments:True"
    This reverts commit 986c7df9004d5bba1dc566b4d580cb110276d68c.

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set -xe
 
-export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 cd ${_tapisExecSystemInputDir}
 insanely-fast-whisper --file-name ${_tapisExecSystemInputDir}/audio.mp3
